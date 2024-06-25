f46e250b9cbfe32bbb5434ccfc82f9d97108da6a
Byron Smith <Byron59@hotmail.co.uk>
2024-06-25 12:19:11 +0100

    Add fieldnames arguement to csv_to_list function

--- etl.py
+++ etl.py
@@ -1,14 +1,18 @@
 import csv
 
-#Reads from a csv file to create a list of dictionaries
-data_list = []
+#Extract
 
-def csv_to_list(list, path):
+data_list = []
+order = 'datetime', 'location', 'name', 'products', 'total_spending', 'CASH/CARD', 'card_number'
+
+#Reads from a csv file to create a list of dictionaries, takes fieldnames as a arguement
+def csv_to_list(list, path, fieldnames):
     with open(path, 'r') as file:
-        csv_file = csv.DictReader(file)
+        csv_file = csv.DictReader(file, fieldnames=fieldnames)
         for row in csv_file:
             list.append(row) 
     return list
 
-leeds_data = csv_to_list(data_list, 'leeds.csv')
-print(leeds_data)
+leeds_data = csv_to_list(data_list, 'leeds.csv', order)
+for dict in leeds_data:
+    print(dict)
