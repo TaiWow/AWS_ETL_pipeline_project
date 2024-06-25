3d924477f90cb4b9f32d94d00a8e3840843176dd
Byron Smith <Byron59@hotmail.co.uk>
2024-06-25 11:38:06 +0100

    Extract leeds data to list

--- etl.py
+++ etl.py
@@ -1,9 +1,14 @@
 import csv
 
 #Reads from a csv file to create a list of dictionaries
+data_list = []
+
 def csv_to_list(list, path):
     with open(path, 'r') as file:
         csv_file = csv.DictReader(file)
         for row in csv_file:
             list.append(row) 
-    return list 
+    return list
+
+leeds_data = csv_to_list(data_list, 'leeds.csv')
+print(leeds_data)
