5b6e28183d8e17b561071d02ea350b9a1239cc4f
haar1sham1d <haaris_ham1d@hotmail.com>
2024-06-25 16:05:05 +0100

    commas

--- Nubi_setup.sql
+++ Nubi_setup.sql
@@ -4,7 +4,7 @@ CREATE TABLE Products (
 Product_id INT NOT NULL AUTO_INCREMENT,
 Product_name VARCHAR(100) NOT NULL,
 Product_price DECIMAL(10,2) NOT NULL,
-PRIMARY KEY (Product_id)
+PRIMARY KEY (Product_id),
 );
 
 CREATE TABLE Transactions (
@@ -12,22 +12,22 @@ Transaction_id INT NOT NULL AUTO_INCREMENT,
 Transaction_date DATE NOT NULL,
 Transaction_time TIME NOT NULL,
 Total_spent DECIMAL(10,2) NOT NULL,
-PRIMARY KEY (Transaction_id)
+PRIMARY KEY (Transaction_id),
 );
 
 CREATE TABLE Customers (
 Customer_id INT NOT NULL AUTO_INCREMENT,
 Customer_location VARCHAR(100) NOT NULL,
-PRIMARY KEY (Transaction_id)
+PRIMARY KEY (Customer_id),
 );
 
 CREATE TABLE Orders (
-Order_id INT NOT NULL AUTO_INCREMENT
+Order_id INT NOT NULL AUTO_INCREMENT,
 Product_id INT NOT  NULL,
 Transaction_id INT NOT NULL,
-PRIMARY KEY (Order_id)
-FOREIGN KEY (Product_id) REFERENCES Orders(Order_id)
-FOREIGN KEY (Transaction_id) REFERENCES Transactions(Transaction_id)
+PRIMARY KEY (Order_id),
+FOREIGN KEY (Product_id) REFERENCES Products(Product_id),
+FOREIGN KEY (Transaction_id) REFERENCES Transactions(Transaction_id),
 );
 
 
