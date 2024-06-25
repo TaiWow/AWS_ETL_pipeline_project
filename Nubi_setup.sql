5089d727a14be1a878866a14f2678d0db934ea43
haar1sham1d <haaris_ham1d@hotmail.com>
2024-06-25 16:08:09 +0100

    if not exists -tables

--- Nubi_setup.sql
+++ Nubi_setup.sql
@@ -1,13 +1,13 @@
 CREATE DATABASE IF NOT EXISTS Nubi_project;
 
-CREATE TABLE Products (
+CREATE TABLE IF NOT EXISTS Products (
 Product_id INT NOT NULL AUTO_INCREMENT,
 Product_name VARCHAR(100) NOT NULL,
 Product_price DECIMAL(10,2) NOT NULL,
 PRIMARY KEY (Product_id),
 );
 
-CREATE TABLE Transactions (
+CREATE TABLE IF NOT EXISTS Transactions (
 Transaction_id INT NOT NULL AUTO_INCREMENT,
 Transaction_date DATE NOT NULL,
 Transaction_time TIME NOT NULL,
@@ -15,13 +15,13 @@ Total_spent DECIMAL(10,2) NOT NULL,
 PRIMARY KEY (Transaction_id),
 );
 
-CREATE TABLE Customers (
+CREATE TABLE IF NOT EXISTS Customers (
 Customer_id INT NOT NULL AUTO_INCREMENT,
 Customer_location VARCHAR(100) NOT NULL,
 PRIMARY KEY (Customer_id),
 );
 
-CREATE TABLE Orders (
+CREATE TABLE IF NOT EXISTS Orders (
 Order_id INT NOT NULL AUTO_INCREMENT,
 Product_id INT NOT  NULL,
 Transaction_id INT NOT NULL,
