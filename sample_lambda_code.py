9d3639e7bad9c3dd43040a87ad54cfb7b74180c4
haar1sham1d <haaris_ham1d@hotmail.com>
2024-07-03 16:09:03 +0100

    lambda stuff

---
+++ sample_lambda_code.py
@@ -0,0 +1,36 @@
+from connect_to_db import *
+import os
+from load import perform_etl
+
+# Name of the environment variable which has the SSM parameter name as its value.
+# The SSM parameter name will be "<team name>_redshift_settings".
+ssm_env_var_name = 'nubi_redshift_settings'
+
+def lambda_handler(event, context):
+    print('lambda_handler: starting')
+
+    try:
+
+        nubi_redshift_settings = os.environ[ssm_env_var_name] or 'NOT_SET'
+        print(f'lambda_handler: nubi_redshift_settings={nubi_redshift_settings} from ssm_env_var_name={ssm_env_var_name}')
+
+        # connection
+        redshift_details = get_ssm_param(nubi_redshift_settings)
+        conn, cur = open_sql_database_connection_and_cursor(redshift_details)
+        
+        #call ETL
+        perform_etl()
+
+        print(f'lambda_handler: done')
+
+    except Exception as whoopsy:
+        # ...exception reporting
+        print(f'lambda_handler: failure, error=${whoopsy}')
+        raise whoopsy
+    
+    finally:
+        # Ensure cursor and connection are closed
+        if cur:
+            cur.close()
+        if conn:
+            conn.close()
