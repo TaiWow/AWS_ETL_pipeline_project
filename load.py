0aa4d256dfc866d3670d6f7c8a93936a91aa3d5a
haar1sham1d <haaris_ham1d@hotmail.com>
2024-07-03 16:08:15 +0100

    lambda stuff

--- load.py
+++ load.py
@@ -6,8 +6,7 @@ from insert_transactions_table import process_transactions
 from insert_orders_table import process_orders
 from create_drop_db_tables import create_db_tables
 
-
-if __name__ == '__main__':
+def perform_etl():
     # Set up the database connection
     connection = db_connection.setup_db_connection()
 
@@ -15,31 +14,31 @@ if __name__ == '__main__':
         create_db_tables(connection)
 
         cursor = connection.cursor()
-        
-        # load and transform CSV data
+
+        # Load and transform CSV data
         leeds_data = csv_transform.csv_to_list('leeds.csv')
         chesterfield_data = csv_transform.csv_to_list('chesterfield_25-08-2021_09-00-00.csv')
-       
+
         combined_data = leeds_data + chesterfield_data
-        
+
         if combined_data:
-            # applied transformation steps 
+            # Applied transformation steps
             transformed_data = csv_transform.remove_sensitive_data(combined_data)
             transformed_data = csv_transform.split_date_and_time(transformed_data)
             transformed_data = csv_transform.split_items_into_list(transformed_data)
-            
+
             # Process and insert the transformed data into the database
             process_products_list(cursor, transformed_data)
             process_locations(cursor, transformed_data)
             process_transactions(cursor, transformed_data)
-            process_orders(cursor,transformed_data)
+            process_orders(cursor, transformed_data)
 
+        connection.commit()
 
-          
-           
-    
-    connection.commit()
-            
-   
-    cursor.close()
-    connection.close()
+        cursor.close()
+        connection.close()
+
+        print('ETL process completed successfully')
+
+if __name__ == '__main__':
+    perform_etl()
